a8f9f09a35943e4a18190649229589fcf96017e5
Kota <maedakoutaa@gmail.com>
2022-04-09 13:43:44 +0900

    Initial Commit

---
+++ MiniApp39-Charts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp39-Charts
+//
+//  Created by 前田航汰 on 2022/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
